419442c5604eeda60abd1f2a17dfd2da11926079
KIN MAX PIAMOLINI GUSMAO <16104046@l1801356.portoalegre.pucrsnet.br>
2016-05-16 11:39:32 -0300

    trabalho

--- WordCount/wc.c
+++ WordCount/wc.c
@@ -1,25 +1,34 @@
+#include <ctype.h>
 #include <stdio.h>
 
 int main() {
 
 	FILE *arquivo;
 	int c, cl = 0, cb = 0, cp = 0;
+	char fname[51];
 
-	arquivo = fopen("fatorial.c", "r");
+	printf ("\n Digite o nome completo do arquivo com extensão: ");
+	scanf ("%s", fname);
+	printf ("\n\n");	
+	arquivo = fopen(fname, "r");
 
-	int i;
+	int i = 0;
+	c = getc(arquivo);
 	while (c != -1) {
 		cb++;		
 		
 		if (c == '\n')
 			cl++;
 		if (c == ' ')
+			i++;
+		if (i == 1)
 			cp++;
+		if (isgraph(c) == 0)
+			i = 0;
 		printf("%i %c %x\n", c, c, c);
 		c = getc(arquivo);
 	}
-
-	cb--;	
+	
 	printf ("L = %d B = %d, P = %d\n", cl, cb, cp);	
 		
 	fclose(arquivo);
